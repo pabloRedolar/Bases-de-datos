fd65c215db64d16ecab1a038263c4c449a55da47
pabloRedolar <predolars@iesch.org>
2024-05-13 21:11:02 +0200

    Ejer 4,5,6 examen

---
+++ Practica examen/Ejer5.sql
@@ -0,0 +1,21 @@
+DROP TRIGGER eliminarPistaCerradaPublico;
+
+DELIMITER //
+
+CREATE TRIGGER eliminarPistaCerradaPublico
+ 
+BEFORE DELETE ON pistas_abiertas FOR EACH ROW
+
+BEGIN
+
+INSERT INTO pistas_cerradas (id_pista, fecha_clausura, motivo) VALUES (OLD.id_pista, NOW(), 'Se ha rompido');
+
+END//
+
+DELIMITER ;
+
+
+DELETE FROM pistas_abiertas WHERE id_pista = 199;
+
+
+
